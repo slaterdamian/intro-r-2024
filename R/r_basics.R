b45715ec829addd14717750f243f77c8c906a720
slaterdamian <slaterdamian@gmail.com>
2024-09-10 15:17:41 -0700

    Discussion about operators

--- R/r_basics.R
+++ R/r_basics.R
@@ -12,6 +12,47 @@ FALSE
 
 # Operators ----
 
+  ## Ar ithmetic
+2 + 3 #note spacing: legibility
+2 - 3
+2 * 3 # multiplication
+2 / 3
+2 ** 3 # exponent
+2 ^ 3
+
+  ## Comparison
+2 == 2 # tests for equality, '=' for defining variable
+2 == (1 + 1)
+"joe" -- "joe" # case-sensitive
+
+2 != 1 # '!' negates whatever follows
+
+2 < 3
+3 > 2
+3 <= 2
+2 >= 3
+
+True == 1
+False == 0
+isTRUE(TRUE) # function testing if the argument is literally TRUE
+isTRUE(1) # 🙃
+?isTRUE() # queries built-in help/wiki
+
+2 < 3 & 2 > 3 # & not 'AND'
+2 < 3 | 2 > 3 # | not 'OR'
+1 == 1 & 1 < 2 | 2 < 3 # how to handle nested 'test?'
+1 == 1 & (1 < 2 | 2 < 3) # with ()
+
+  ## Type matters (sometimes)
+"joe" # string or a character
+typeof("joe")
+42 # numeric (double precision, floating point)
+typeof(42)
+TRUE # logical/boolean
+typeof(TRUE)
+
+42 == "42" # equality can cross types
+identical(42, "42")
 
 # variables ----
 
